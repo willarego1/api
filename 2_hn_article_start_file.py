2ef7dcedbdd1b01ce0bf310f5d1df461df637151
JB <me@me.com>
2024-04-04 12:19:05 -0500

    updated hn file

--- 2_hn_article_start_file.py
+++ 2_hn_article_start_file.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # Make an API call, and store the response.
-url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 r = requests.get(url)
 print(f"Status code: {r.status_code}")
 
