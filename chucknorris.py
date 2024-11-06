a72c2171a415ce28baef3852a2b1c6181180b412
JB <me@me.com>
2024-11-06 13:57:31 -0600

    added chuck norris

---
+++ chucknorris.py
@@ -0,0 +1,50 @@
+import requests
+import json
+
+
+#random chuch norris jokes
+url = "https://api.chucknorris.io/jokes/random"
+
+
+#list of categories
+url = "https://api.chucknorris.io/jokes/categories"
+
+
+#random joke from a specific category
+url = "https://api.chucknorris.io/jokes/random?category={category}"
+
+
+#text search
+url = "https://api.chucknorris.io/jokes/search?query={query}"
+
+
+
+
+'''
+Part I
+The program should welcome the user by displaying a random chuch norris joke
+'''
+
+
+
+'''
+Part II
+list the categories to the user and ask to pick a category
+'''
+
+
+
+'''
+Part III
+Display a joke based on the category picked by the user
+'''
+
+
+
+'''
+Part IV
+See if you can find a match for the user's favorite chuck norris joke
+by asking the user to enter in a few key words of the joke
+'''
+
+
